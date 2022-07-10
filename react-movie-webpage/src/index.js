9203064b1d4bbc2c0ebdebe22cc0a2d1b6922005
nostrss <jintagi@gmail.com>
2022-07-11 01:01:41 +0900

    section 7-5

--- react-movie-webpage/src/index.js
+++ react-movie-webpage/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import TodoList from './todo';
-import CoinTracker from './coin-tracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <TodoList />
-    <CoinTracker />
   </React.StrictMode>
 );
