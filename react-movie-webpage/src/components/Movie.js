b9aa7b5feb0276da19f6292cb98d6f92abd46b14
nostrss <jintagi@gmail.com>
2022-07-11 16:45:32 +0900

    add css

--- react-movie-webpage/src/components/Movie.js
+++ react-movie-webpage/src/components/Movie.js
@@ -1,22 +1,26 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './Movie.module.css';
+
 Movie.propTypes = {
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
 };
 
-export default function Movie({ id, coverImg, title, summary, genres }) {
+export default function Movie({ year, id, coverImg, title, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <h2 className={styles.movie__title}>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
-      <ul>
+      <h3 className={styles.movie__year}>{year}</h3>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+      <ul className={styles.movie__genres}>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
